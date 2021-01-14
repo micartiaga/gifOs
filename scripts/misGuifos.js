59263f3b1851b413d22c077639aea64e5c0f3498
micartiaga <micaela.artiaga@gmail.com>
2021-01-14 20:37:47 -0300

    creando guifos

--- scripts/misGuifos.js
+++ scripts/misGuifos.js
@@ -1,3 +1,11 @@
 // CREAR GUIFOS BOTONES PASO 1
 
-const comenzar=document.getElementById('misGuifosUnoCancelar');
+const cancelarBtn1=document.getElementById('misGuifosUnoCancelar');
+const comenzarBtn= document.getElementById('misGuifosUnoComenzar');
+const ventanaInstrucciones=document.getElementById('windowCrearGuifos');
+
+// CREAR GUIFOS PASO 1
+
+cancelarBtn1.addEventListener("click", ()=>{
+    window.location.href= 'misGuifos.html';
+})
